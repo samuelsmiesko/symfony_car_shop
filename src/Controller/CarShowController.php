58172758b9e9a4e1be9cb2c4bb269c0d5a2c98d3
samoprogramuje <samuel1820@gmail.com>
2024-05-07 21:06:51 +0200

    ajax finish pages

--- src/Controller/CarShowController.php
+++ src/Controller/CarShowController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 use App\Entity\Cars;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,20 +17,70 @@ class CarShowController extends AbstractController
         $this->em = $em;
     }
 
-    #[Route('/', name: 'app_car_show')]
-    public function index(): Response
-    {
+    // #[Route('/', name: 'app_car_show')]
+    // public function index(): Response
+    // {
 
         
-            $lists = $this->em->getRepository(Cars::class)->findBy(
-                array(),
-                array('id' => 'DESC'),
-                40,
-                0
-            );
-        return $this->render('car_show/index.html.twig', [
-            'lists' => $lists,
-        ]);
+    //         $lists = $this->em->getRepository(Cars::class)->findBy(
+    //             array(),
+    //             array('id' => 'ASC'),
+    //             10,
+    //             0
+    //         );
+    //     return $this->render('car_show/index.html.twig', [
+    //         'lists' => $lists,
+    //     ]);
         
+    // }
+
+
+    #[Route("/")]
+    
+    public function ajaxAction(Request $request) {  
+
+        
+        if(isset($_REQUEST['get_variable'])){
+            $limit = $_REQUEST['get_variable'];
+        }else{
+            $limit = 1;
+        }
+
+        $TopLimit = $limit * 5;
+        $BottomLimit = ($limit * 5)-4;
+
+        $lists = $this->em->getRepository(Cars::class)->findAll();
+        
+        $students = $this->em->getRepository(Cars::class)->findBy(
+            array(),
+            array('id' => 'ASC'),
+            $TopLimit,
+            $BottomLimit
+        ); 
+        
+        if ($request->isXmlHttpRequest() || $request->query->get('showJson') == 1) {  
+        $jsonData = array();  
+        $idx = 0;  
+        foreach($students as $student) {  
+            $temp = array(
+                'id' => $student->getId(),  
+                'brandname' => $student->getBrandname(),  
+                'model' => $student->getModelname(), 
+                'modelyear' => $student->getModelyear(), 
+                'milage' => $student->getMilage(), 
+                'price' => $student->getPrice(), 
+                'status' => $student->getStatus(),
+                'image' => $student->getimage(), 
+            );   
+            $jsonData[$idx++] = $temp;  
+        } 
+        return new JsonResponse($jsonData); 
+        } else { 
+            return $this->render('car_show/index.html.twig', [
+                'lists' => $lists,
+                
+  
+            ]);
+        } 
     }
 }
