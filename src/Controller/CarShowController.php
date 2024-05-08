1a426218dc6b25bbf766265dc90931c2a29275bb
samoprogramuje <samuel1820@gmail.com>
2024-05-08 19:56:21 +0200

    add query

--- src/Controller/CarShowController.php
+++ src/Controller/CarShowController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Finder\Finder;
 class CarShowController extends AbstractController
 {
 
@@ -50,18 +51,42 @@ class CarShowController extends AbstractController
         $BottomLimit = ($limit * 5)-4;
 
         $lists = $this->em->getRepository(Cars::class)->findAll();
+
+
+        $dir    = 'carfotos/car1/';
+        $files = scandir($dir);
+        array_splice($files, 0, 2);
         
+        $array_lenght = count($files);
+        //echo($array_lenght);
+        //echo($files[1]);
+        
+
         $students = $this->em->getRepository(Cars::class)->findBy(
             array(),
             array('id' => 'ASC'),
             $TopLimit,
             $BottomLimit
         ); 
-        
+
+        //print_r($students[0]);
+
+        // foreach($students as $student) {  
+        //     $dir = $student->getimage();
+            
+        //     $files = scandir($dir);
+        //     print_r($dir.$files[2]);
+        //     // array_splice($files, 0, 2);
+            
+        // }
+
         if ($request->isXmlHttpRequest() || $request->query->get('showJson') == 1) {  
         $jsonData = array();  
         $idx = 0;  
-        foreach($students as $student) {  
+        foreach($students as $student) { 
+            $dir = $student->getimage();
+            $files = scandir($dir);
+            $imageToDisplay=$dir.$files[2]; 
             $temp = array(
                 'id' => $student->getId(),  
                 'brandname' => $student->getBrandname(),  
@@ -70,10 +95,14 @@ class CarShowController extends AbstractController
                 'milage' => $student->getMilage(), 
                 'price' => $student->getPrice(), 
                 'status' => $student->getStatus(),
-                'image' => $student->getimage(), 
+                
+                'image' => $imageToDisplay, 
             );   
             $jsonData[$idx++] = $temp;  
+
+            
         } 
+        
         return new JsonResponse($jsonData); 
         } else { 
             return $this->render('car_show/index.html.twig', [
