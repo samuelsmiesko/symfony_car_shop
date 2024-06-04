4699272c59ba2d2fcce6b3b41425f47d1f7bd31f
samoprogramuje <samuel1820@gmail.com>
2024-06-04 14:15:25 +0200

    add range scroll

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -24,6 +24,9 @@ class RegistrationController extends AbstractController
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
+
+        
+
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
@@ -51,7 +54,7 @@ class RegistrationController extends AbstractController
 
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('pagePick');
+            //return $this->redirectToRoute('pagePick');
         }
 
         return $this->render('registration/register.html.twig', [
--- src/Form/SearchFilterType.php
+++ src/Form/SearchFilterType.php
@@ -16,21 +16,35 @@ class SearchFilterType extends AbstractType
         $builder
 
 
-            ->add('Price',RangeType::class,[
-                'mapped' => true,  
+            ->add('From',RangeType::class,[
+                'mapped' => false,  
                 'attr' => [
                     
                     'class' => 'form-range',
                     'type' => 'range',
-                    'id' => 'customRange',
-                    
+                    'id' => 'from',
                     'min'=> "100",
                     'max'=> "100000",
                     'step'=> "100",
-                    
-                    
+                    'value' => "1000",
                 ],
             ])
+
+            ->add('To',RangeType::class,[
+                'mapped' => false,  
+                'attr' => [
+                    
+                    'class' => 'form-range',
+                    'type' => 'range',
+                    'id' => 'to',
+                    'min'=> "100",
+                    'max'=> "100000",
+                    'step'=> "100",
+                    'value' => "10000",
+                ],
+            ])        
+
+
 
             ->add('Used',CheckboxType::class,[
                 'mapped' => false,  
